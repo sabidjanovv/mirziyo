c9f00798f4dec474144ff7113cc5eaffd3462c0c
sabidjanovv <sabidjanov@gmail.com>
2024-12-12 11:43:06 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import mirziyo from "./assets/mirziyo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img src={mirziyo} alt="" />
+    </div>
+  );
+}
+
+export default App;
